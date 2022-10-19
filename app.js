97dd1aa87dd8db85a4e0f6a448cd2fea3cdc186d
Gi Leite <giovanna.mleite@hotmail.com>
2022-10-18 21:07:44 -0300

    javascript

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Olá, estou funcionando")
+console.log(10+3-5)
+console.log(10*2)
+console.log(500/2)
+console.log(18-8)
+// 
+let nome= "Gi"
+let nota1 = 9.1
+let nota2 = 7.9
+let nota3= 4.88
+let nota4 = 2.4
+let media =(nota1 + nota2 + nota3 + nota4)/ 4
+let mediaFixada = media.toFixed(1)
+
+console.log("Olá + nome")
+console.log("Sua média foi" + media)
